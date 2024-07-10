256d3315301056d27357e472b3ca7645a5544eda
Banuubalta <nbanu.balta@gmail.com>
2024-07-11 02:35:48 +0300

    first commit

---
+++ src/main/java/com/banubalta/repository/TeacherRepository.java
@@ -0,0 +1,9 @@
+package com.banubalta.repository;
+
+import com.banubalta.model.Teacher;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TeacherRepository extends JpaRepository<Teacher, Long> {
+}
